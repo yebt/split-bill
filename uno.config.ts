0c2a88de950429a78940b3b4f72009b6c7da8ad2
Yahir <yahir.eduardo.bravo@gmail.com>
2025-09-30 23:08:01 -0500

    feat: add unocss preset for tawild4 compatibility class

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,16 @@
+import presetWind4 from '@unocss/preset-wind4'
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
   // ...UnoCSS options
+  presets: [
+    presetWind4({
+      preflights: {
+        reset: true,
+      },
+      dark: 'class',
+    }),
+  ],
   content: {
     pipeline: {
       include: [
