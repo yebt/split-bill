a3e9f820719972289bbeaa15c2b0c553207775f6
Yahir <yahir.eduardo.bravo@gmail.com>
2025-10-01 23:49:54 -0500

    feat: add unhead

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { useThemeStore } from '@/stores/themeStore'
+import { useHead } from '@unhead/vue'
 
 // Initialize theme store
 useThemeStore()
+// i try use the basics head
+useHead({
+  bodyAttrs:{
+    style: 'background: salmon; color: cyan;'
+  },
+  meta: [
+    { name: 'description' , content: 'App to management Parches and bills' },
+    // og
+
+  ]
+})
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createHead } from '@unhead/vue/client'
 
 import App from './App.vue'
 import router from './router'
@@ -10,6 +11,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(createHead())
 app.use(router)
 
 app.mount('#app')
