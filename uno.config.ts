259cc687e5a49d7ab66fa2e18d30650b00dac7bf
Yahir <yahir.eduardo.bravo@gmail.com>
2025-10-08 17:16:56 -0500

    fix: change the transformer for the directives

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,4 @@
-import { transformerDirectives } from '@unocss/transformer-directives'
+import transformerDirectives from '@unocss/transformer-directives'
 import transformerVariantGroup from '@unocss/transformer-variant-group'
 import presetIcons from '@unocss/preset-icons'
 import presetWind4 from '@unocss/preset-wind4'
